387d1c107a88faad6bc33b8fe0d32e68d5aaec39
lemokami <jesswinw@gmail.com>
2022-10-02 02:04:20 +0530

    feat: add mdx and solidjs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
 
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), solidJs(), mdx()]
 });
