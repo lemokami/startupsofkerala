bfe312bc69ee95f4b7462e9c8ab09d20165a3539
lemokami <jesswinw@gmail.com>
2022-10-03 22:25:06 +0530

    feat: add astrojs/image package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), solidJs(), mdx()]
+  }), solidJs(), mdx(), image()]
 });
