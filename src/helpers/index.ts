731c17dab678a4e4df2e96c838390c59558fec05
lemokami <jesswinw@gmail.com>
2022-10-04 00:27:42 +0530

    feat: add getImagePath helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,8 @@
+import logoList from '../constants/logoMap.json';
 export const generateRoutename = (fname: string) => {
   return fname.split('/')[1].replace('.mdx', '');
+};
+
+export const getImagePath = (key: string) => {
+  return logoList[key];
 };
