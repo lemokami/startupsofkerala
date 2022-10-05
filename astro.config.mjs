4a864bdc0da44a9dace8be6bc17967ee67c50c94
lemokami <jesswinw@gmail.com>
2022-10-06 01:25:10 +0530

    feat: add sitemap and robots plugin and necessary config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import mdx from "@astrojs/mdx";
-
-import image from "@astrojs/image";
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), solidJs(), mdx(), image()]
+  site: 'https://startupsofkerala.in',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    solidJs(),
+    mdx(),
+    image(),
+    sitemap(),
+    robotsTxt(),
+  ],
 });
