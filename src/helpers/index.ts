659614744dcaab1f8a182b8899abf1b725235909
lemokami <jesswinw@gmail.com>
2022-10-24 23:58:09 +0530

    fix: file sorting issue

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,3 +6,11 @@ export const generateRoutename = (fname: string) => {
 export const getImagePath = (key: string) => {
   return logoList[key.toLowerCase()];
 };
+
+export function asciiSort(a: string, b: string) {
+  if (a.toLowerCase() < b.toLowerCase()) {
+    return -1;
+  } else if (a.toLowerCase() == b.toLowerCase()) {
+    return 0;
+  } else return 1;
+}
